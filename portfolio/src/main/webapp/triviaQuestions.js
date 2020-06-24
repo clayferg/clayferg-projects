beb88cda5edb5fcb57af0f07b9161a6390f02659
Clayton <claytonferg01@gmail.com>
2020-06-24 19:15:33 +0000

    created function path for not playing

--- portfolio/src/main/webapp/triviaQuestions.js
+++ portfolio/src/main/webapp/triviaQuestions.js
@@ -1,3 +1,12 @@
 /**
  * Prompts questions about me and updates the score
  */
+
+ function triviaQuestions(wannaPlay) {
+ if (wannaPlay) {
+
+ } else {
+     alert("Hm...your loss"); 
+     document.getElementById(pointTracker).innerHTML = "Final Score: 0/10"; 
+ }
+ }
