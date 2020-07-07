0b33476618aa365c91b44f4f355dd94795327360
Clayton <claytonferg01@gmail.com>
2020-07-07 17:23:24 +0000

    Use online beautifier on script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,26 +11,25 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 /**
  * Shows random facts about me until there are none left
  */
-const facts =
-  [
-    'My favorite TV shows are Atlanta (FX), Rick and Morty (AS), and Bojack' +
-    'Horseman (Netflix). You do not have to watch them,' +
-    'but you also kind of do.',
-    ' My favorite albums are Fulfillingness\' First Finale (Stevie Wonder),' +
-    + ' Becuase the Internet (Childish Gambino), To Pimp A Butterfly ' +
-    ' (Kendrick Lamar), 2014 Forrest Hills Drive (J Cole),' +
-    ' and Acid Rap (Chance The Rapper).',
-    'My favorite movies are Get Out, The Artist, and Sorry To Bother You.',
-    'Mixed Bag: favorite time - 9:30 A.M., favorite food - steak' +
-    '(oops, you knew that already), favorite color - blue!',
-  ];
+const facts = [
+  'My favorite TV shows are Atlanta (FX), Rick and Morty (AS), and Bojack' +
+  'Horseman (Netflix). You do not have to watch them,' +
+  'but you also kind of do.',
+  ' My favorite albums are Fulfillingness\' First Finale (Stevie Wonder),' + 
+  ' Becuase the Internet (Childish Gambino), To Pimp A Butterfly ' +
+  ' (Kendrick Lamar), 2014 Forrest Hills Drive (J Cole),' +
+  ' and Acid Rap (Chance The Rapper).',
+  'My favorite movies are Get Out, The Artist, and Sorry To Bother You.',
+  'Mixed Bag: favorite time - 9:30 A.M., favorite food - steak' +
+  '(oops, you knew that already), favorite color - blue!',
+];
+
 function addFavoriteThing() {
   const factContainer = document.getElementById('greeting-container');
-  if (facts.length!=0) {
+  if (facts.length != 0) {
     // Pick a random fact.
     const randomNumber = Math.floor(Math.random() * facts.length);
     const fact = facts[randomNumber];
@@ -44,254 +43,223 @@ function addFavoriteThing() {
 // Retreive Messages from Servlet
 function getMessage(maxNum) {
   if (maxNum == null || maxNum == "") {
-      maxNum = 5; 
+    maxNum = 5;
   }
-  fetch('/data?max-num-comments=' + maxNum).then((response) => response.json()).then((messages) => {
-    const messageElement = document.getElementById('message-container');
-    messageElement.innerHTML = '';
-    messages.forEach(getMessageHelper);
-  })
-  }
+  fetch('/data?max-num-comments=' + maxNum).then((response) => response.json())
+    .then((messages) => {
+      const messageElement = document.getElementById('message-container');
+      messageElement.innerHTML = '';
+      messages.forEach(getMessageHelper);
+    })
+}
 
 function getMessageHelper(singleComment) {
   const messageElement = document.getElementById('message-container');
-  const usernameElement = document.createElement('p'); 
-  const commentElement = document.createElement('p');   
-  usernameElement.innerText = "Username: " + singleComment.username;  
-  commentElement.innerText = "Comment: " + singleComment.comment; 
+  const usernameElement = document.createElement('p');
+  const commentElement = document.createElement('p');
+  usernameElement.innerText = "Username: " + singleComment.username;
+  commentElement.innerText = "Comment: " + singleComment.comment;
   messageElement.appendChild(usernameElement);
   messageElement.appendChild(commentElement);
 }
 
 function deleteAllMessages() {
-  const request = new Request('/delete-data', {method: 'POST'});
+  const request = new Request('/delete-data', {
+    method: 'POST'
+  });
   fetch(request).then(response => {
-      getMessage(0);
+    getMessage(0);
   })
 }
 
 function createMap() {
-  const map = new google.maps.Map(
-    document.getElementById('map'), {
-      center: {
-        lat: 41.3492972,
-        lng: -81.4174989,
-      },
-      zoom: 5,
-      styles: [{
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#8ec3b9"
-          }]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1a3646"
-          }]
-        },
-        {
-          "featureType": "administrative.country",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#4b6878"
-          }]
-        },
-        {
-          "featureType": "administrative.land_parcel",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#64779e"
-          }]
-        },
-        {
-          "featureType": "administrative.province",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#4b6878"
-          }]
-        },
-        {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#334e87"
-          }]
-        },
-        {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#283d6a"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#6f9ba5"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#3C7680"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#304a7d"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#98a5be"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "road.arterial",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#2c6675"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#255763"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#b0d5ce"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "road.local",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#98a5be"
-          }]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#283d6a"
-          }]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#3a4762"
-          }]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#0e1626"
-          }]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry.fill",
-          "stylers": [{
-              "color": "#b3e2ff"
-            },
-            {
-              "saturation": -30
-            },
-            {
-              "weight": 1
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#4e6d70"
-          }]
-        }
-      ]
-    });
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 41.3492972,
+      lng: -81.4174989,
+    },
+    zoom: 5,
+    styles: [{
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#8ec3b9"
+      }]
+    }, {
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1a3646"
+      }]
+    }, {
+      "featureType": "administrative.country",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#4b6878"
+      }]
+    }, {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#64779e"
+      }]
+    }, {
+      "featureType": "administrative.province",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#4b6878"
+      }]
+    }, {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#334e87"
+      }]
+    }, {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#283d6a"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#6f9ba5"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#3C7680"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#304a7d"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#98a5be"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "road.arterial",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#2c6675"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#255763"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "labels",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#b0d5ce"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "road.local",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#98a5be"
+      }]
+    }, {
+      "featureType": "transit",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "transit.line",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#283d6a"
+      }]
+    }, {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#3a4762"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#0e1626"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#b3e2ff"
+      }, {
+        "saturation": -30
+      }, {
+        "weight": 1
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#4e6d70"
+      }]
+    }]
+  });
   const homeMarker = new google.maps.Marker({
     position: {
       lat: 41.3492972,
@@ -300,12 +268,9 @@ function createMap() {
     title: "My Home"
   });
   homeMarker.setMap(map);
-  const homeMarkerContent = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
-    '<h1 id="firstHeading" class="firstHeading">My Home</h1>' +
-    '<div id="bodyContent">' +
-    '<p> Here is info about my house </p>' +
+  const homeMarkerContent = '<div id="content">' + '<div id="siteNotice">' +
+    '</div>' + '<h1 id="firstHeading" class="firstHeading">My Home</h1>' +
+    '<div id="bodyContent">' + '<p> Here is info about my house </p>' +
     '<p>Here is where I may put a link: <a href="index.html">' +
     'Link</a> </p> </div> </div>';
   const homeInfoWindow = new google.maps.InfoWindow({
@@ -322,12 +287,9 @@ function createMap() {
     title: "My University"
   });
   schoolMarker.setMap(map);
-  const schoolMarkerContent = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
-    '<h1 id="firstHeading" class="firstHeading">My School</h1>' +
-    '<div id="bodyContent">' +
-    '<p> Here is info about Princeton </p>' +
+  const schoolMarkerContent = '<div id="content">' + '<div id="siteNotice">' +
+    '</div>' + '<h1 id="firstHeading" class="firstHeading">My School</h1>' +
+    '<div id="bodyContent">' + '<p> Here is info about Princeton </p>' +
     '<p>Here is where I may put a link: <a href="index.html">' +
     'Link</a> </p> </div> </div>';
   const schoolInfoWindow = new google.maps.InfoWindow({
@@ -345,14 +307,11 @@ function createMap() {
   });
   highSchoolMarker.setMap(map);
   const highSchoolMarkerContent = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
+    '<div id="siteNotice">' + '</div>' +
     '<h1 id="firstHeading" class="firstHeading">My High School</h1>' +
-    '<div id="bodyContent">' +
-    '<p> Here is info about My High School</p>' +
+    '<div id="bodyContent">' + '<p> Here is info about My High School</p>' +
     '<p>Here is where I may put a link: <a href="index.html">' +
     'Link</a> </p> </div> </div>';
-
   const highSchoolInfoWindow = new google.maps.InfoWindow({
     content: highSchoolMarkerContent
   });
@@ -367,12 +326,10 @@ function createMap() {
     title: "My Favorite Restaurant (Five Guys)"
   });
   fiveGuysMarker.setMap(map);
-  const fiveGuysMarkerContent = '<div id="content">' +
-    '<div id="siteNotice">' +
+  const fiveGuysMarkerContent = '<div id="content">' + '<div id="siteNotice">' +
     '</div>' +
     '<h1 id="firstHeading" class="firstHeading">My Favorite Restaurant</h1>' +
-    '<div id="bodyContent">' +
-    '<p> Here is info about Five Guys </p>' +
+    '<div id="bodyContent">' + '<p> Here is info about Five Guys </p>' +
     '<p>Here is where I may put a link: <a href="index.html">' +
     'Link</a> </p> </div> </div>';
   const fiveGuysInfoWindow = new google.maps.InfoWindow({
@@ -389,12 +346,10 @@ function createMap() {
     title: "Clevleand Basektball Arena"
   });
   rMFHMarker.setMap(map);
-  const rMFHMarkerContent = '<div id="content">' +
-    '<div id="siteNotice">' +
+  const rMFHMarkerContent = '<div id="content">' + '<div id="siteNotice">' +
     '</div>' +
     '<h1 id="firstHeading" class="firstHeading">Rocket Mortgage FieldHouse</h1>' +
-    '<div id="bodyContent">' +
-    '<p> Here is info about RMFH. </p>' +
+    '<div id="bodyContent">' + '<p> Here is info about RMFH. </p>' +
     '<p>Here is where I may put a link: <a href="index.html">' +
     'Link</a> </p> </div> </div>';
   const rMFHInfoWindow = new google.maps.InfoWindow({
