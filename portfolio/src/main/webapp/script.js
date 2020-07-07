e7ee2b8365b69f7c942628f1622c91baf6e0536e
Clayton <claytonferg01@gmail.com>
2020-07-07 13:30:05 +0000

    Show a Google Map on about me page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -69,3 +69,9 @@ function deleteAllMessages() {
       getMessage(0);
   })
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
